c77f4ee2092529b4a0560149f7b1142b74e393f6
Awesometimei <dtnguyen194@student.hbuhsd.edu>
2024-10-01 10:17:33 -0700

    danny

--- Classwork.java
+++ Classwork.java
@@ -1,7 +1,27 @@
+import java.util.Scanner;
+
 public class Classwork
 {
   public static void main(String[] args)
   {
-    // write your code
+    Shopping();
   }
+
+  public static void Shopping()
+  {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("What type of item are you buying?");
+    String itemType = sc.nextLine();
+    System.out.println("How many are you buying?");
+    int itemQuantity = sc.nextInt();
+    System.out.println("How much do each weigh?");
+    double itemWeight = sc.nextDouble();
+  }
+
+  public static void Quote()
+  {
+    System.out.println("\"That brain")
+  }
+
 }
+
